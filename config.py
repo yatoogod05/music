b21255d6efbb9a58e1cc8f54e663dc34f134577f
Raghav-afk25 <raghavagarwalq12345@gmail.com>
2024-12-25 12:50:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,9 +35,9 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AnonymousX1025/AnonXMusic",
+    "https://github.com/Raghav-afk25/Void",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
